b9c3dbe4998368914ec4162f57710fb72df31cc8
Xfunnelbuilder <89966765+Xfunnelbuilder@users.noreply.github.com>
2021-09-02 23:11:16 +0100

    Create carousel.js

---
+++ carousel.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+      $("[data-title='carousel-header1'], .carousel-header1").after("<div class='owl-carousel mycarousel1 owl-theme'></div>");
+      $("[data-title='carousel-header2'], .carousel-header2").after("<div class='owl-carousel mycarousel2 owl-theme'></div>");
+      $("[data-title='carousel-header3'], .carousel-header3").after("<div class='owl-carousel mycarousel2 owl-theme'></div>");
+      $("[data-title='carousel1']").addClass("owl-carousel mycarousel1 owl-theme");
+      $("[data-title='carousel2']").addClass("owl-carousel mycarousel2 owl-theme");
+      $("[data-title='carousel3']").addClass("owl-carousel mycarousel3 owl-theme");
+      $("[data-title='item1']").addClass("item1");
+      $("[data-title='item2']").addClass("item2");
+      $("[data-title='item3']").addClass("item3");
+      $(".mycarousel1").append($(".item1"));
+      $(".mycarousel2").append($(".item2"));
+      $(".mycarousel3").append($(".item3"));
+});
+
+$('[data-title="panel"], .panel').hide(); // hide panel 
+  $('[data-title="accordion"], .accordion').click(function(){
+    var panel = $(this).next(); // select panel next to the accordion
+    if (panel.css('display')=='block'){
+    $(".active").removeClass("active"); // remove active class from the accordion
+      panel.slideUp(); // to slide up the panel next to the accordion
+    } else if (panel.css('display')=='none'){
+    $(".active").removeClass("active");  
+     $('[data-title="panel"], .panel').slideUp(); // to slide up other panels
+       panel.slideDown(); // to slide down the panel next to the accordion
+      $(this).addClass("active");
+    }
+  });
