ce4fce8708e624c3ca4f68e6a592f9fcfa55e604
Xfunnelbuilder <89966765+Xfunnelbuilder@users.noreply.github.com>
2021-09-02 22:54:20 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha256-pTxD+DSzIwmwhOqTFN+DB+nHjO4iAsbgfyFq5K5bcE0=" crossorigin="anonymous"></script>
